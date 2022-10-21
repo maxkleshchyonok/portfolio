7230fbdb67d41b6bbbed23c4496ac54e215012c7
maxkleshchyonok <56303675+maxkleshchyonok@users.noreply.github.com>
2022-10-21 16:47:51 +0300

    feat: add main shuffle functional

--- codejam-the-gem-puzzle/src/script.js
+++ codejam-the-gem-puzzle/src/script.js
@@ -2,6 +2,26 @@ const containerNode = document.getElementById('puzzle');
 const itemNodes = Array.from(containerNode.querySelectorAll('.puzzle-block'));
 const countItems = 16;
 
+let timer;
+document.getElementById('play').addEventListener('click', () => {
+    // randomSwap(matrix);
+    // setPositionItems(matrix);
+    let shuffleCount = 0;
+    clearInterval(timer);
+    if(shuffleCount === 0){
+        timer = setInterval(()=>{
+            randomSwap(matrix);
+            setPositionItems(matrix);
+            shuffleCount += 1;
+            if(shuffleCount >= 100){
+                clearInterval(timer);
+            }
+        }, 50);
+    }
+})
+
+
+
 let matrix = getMatrix(
     itemNodes.map((item) => Number(item.dataset.matrixId))
 );
@@ -39,11 +59,11 @@ function setNodeStyles(node, x, y) {
     node.style.transform = `translate3D(${x * shiftPs}%, ${y * shiftPs}%, 0)`
 }
 
-document.getElementById('play').addEventListener('click', () => {
-    const shuffledArray = shuffleArray(matrix.flat())
-    matrix = getMatrix(shuffledArray)
-    setPositionItems(matrix)
-})
+// document.getElementById('play').addEventListener('click', () => {
+//     const shuffledArray = shuffleArray(matrix.flat());
+//     matrix = getMatrix(shuffledArray);
+//     setPositionItems(matrix);
+// })
 
 function shuffleArray(arr) {
     return arr
@@ -62,7 +82,7 @@ containerNode.addEventListener('click', (event) => {
     const buttonNumber = Number(buttonNode.dataset.matrixId);
     const buttonCoords = findCoordinatesByNumber(buttonNumber, matrix);
     const blankCoords = findCoordinatesByNumber(blankNumber, matrix);
-    const isValid = isValidForSwap(buttonCoords, blankCoords)
+    const isValid = isValidForSwap(buttonCoords, blankCoords);
     if(isValid){
         swap(blankCoords, buttonCoords, matrix);
         setPositionItems(matrix);
@@ -91,4 +111,58 @@ function swap(coords1, coords2, matrix) {
     const coords1Number = matrix[coords1.y][coords1.x];
     matrix[coords1.y][coords1.x] = matrix[coords2.y][coords2.x];
     matrix[coords2.y][coords2.x] = coords1Number;
+    if(isWin(matrix)){
+        addWinClass();
+    }
+}
+
+function randomSwap(matrix) {
+    const blankCoords = findCoordinatesByNumber(blankNumber, matrix);
+    const validCoords = findValidCoords({
+        blankCoords,matrix, blockedCoords
+    });
+    const swapCoords = validCoords[
+            Math.floor(Math.random()*validCoords.length)
+        ];
+    swap(blankCoords,swapCoords,matrix);
+    blockedCoords = blankCoords;
+}
+
+function findValidCoords({blankCoords, matrix, blockedCoords}) {
+    const validCoords = [];
+    for (let y=0; y<matrix.length;y++){
+        for (let x=0; x<matrix[y].length;x++) {
+            if(isValidForSwap({x, y}, blankCoords)){
+                if(!blockedCoords || !(blockedCoords.x ===x && blockedCoords.y === y)) {
+                    validCoords.push({x, y});
+                }
+            }
+        }
+    }
+    return validCoords;
+}
+
+let blockedCoords = null;
+
+const winFlatArr = new Array(16).fill(0).map((item, i) => i + 1);
+
+function isWin(matrix) {
+    const flatMatrix = matrix.flat();
+    for (let i=0; i<winFlatArr.length; i++){
+        if (flatMatrix[i] !== winFlatArr[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+const winClass = 'win'
+
+function addWinClass() {
+    setTimeout(() => {
+        containerNode.classList.add(winClass)
+    },200);
+    setTimeout(() => {
+        containerNode.classList.remove(winClass)
+    },2000);
 }
